682a5277186448f986daa9c2fc7ec4cbf2231a47
jezler <146593042+oJezler-git@users.noreply.github.com>
2024-09-01 19:43:40 +0100

    add padding to actual container

--- src/components/Get.jsx
+++ src/components/Get.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function Get() {
   return (
-    <div className="bg-[#1a1a1a] text-center flex flex-col justify-center items-center h-[500px] !font-inter">
+    <div className="bg-[#1a1a1a] text-center flex flex-col justify-center items-center h-[500px] !font-inter px-4 md:px-8">
       <h1 className="text-white text-4xl md:text-6xl font-bold mb-4">
         Ready to get TrendTube? Join us today!
       </h1>
-      <p className="text-gray-400 text-lg md:text-xl mb-8 px-4 md:px-8 lg:px-0">
+      <p className="text-gray-400 text-lg md:text-xl mb-8">
         Stop posting bland shorts that have no chance of getting any attention
         on YouTube.
         <br className="hidden lg:block" /> With TrendTube you will be one step
